a967908f2a530abd3d894a1d6324e1037507403e
chrisjterry <chrisjterry102@gmail.com>
2020-03-05 08:51:05 -0800

    Use asset pipeline in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # DO fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
